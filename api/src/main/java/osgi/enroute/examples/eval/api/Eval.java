1cd22d8f88fbf97d07f2bdd3080cd1c78f721900
Krzysztof Gołębiowski <krzysiek.golebiowski@gmail.com>
2017-02-26 18:28:16 +0100

    Added @ProviderType annotation to Eval interface

--- api/src/main/java/osgi/enroute/examples/eval/api/Eval.java
+++ api/src/main/java/osgi/enroute/examples/eval/api/Eval.java
@@ -1,8 +1,11 @@
 package osgi.enroute.examples.eval.api;
+
+import org.osgi.annotation.versioning.ProviderType;
+
 /**
  * A service that evaluates an expression and returns the result
  */
-
+@ProviderType
 public interface Eval {
     /**
      * Evaluate an expression and return the result.
